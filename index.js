ce0d4b00c691d80997ec4895c15eaaeb453a1d35
slam814 <abdussalam6111997@gmail.com>
2024-07-05 11:10:40 +0600

    i added some file and text

---
+++ index.js
@@ -0,0 +1,20 @@
+// Data access
+const data = [{id: 25143, name: 'Abul kaka', address: 'nilfamari kolkata'}];
+
+
+// console.log(Object.keys(data[0]));
+
+
+const products = {
+    countent: 5000,
+    data: [
+        {id: 1, name: 'lenovo think pad', price: 65000},
+        {id: 2, name: 'Mackbook', price: 165000},
+        
+    ]
+}
+
+// second product price
+
+// console.log(products.data[1].price);
+console.log(products.data[0]?.ram?.price.cpu);
